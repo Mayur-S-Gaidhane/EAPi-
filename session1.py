b08eac0e99078d172095833c063ff3ea1cf0b00c
Mayur-S-Gaidhane <sahebrao21@gmail.com>
2021-01-08 21:18:12 +0530

    1st commit

--- session1.py
+++ session1.py
@@ -3,10 +3,10 @@ def myFunc():
     n=len(string)
     arr=[]
     for i in range(n):
-      for j in range(i+1,n+1):
-          a=string[i:j]
-          arr.append(a)
-	print(arr)
+        for j in range(i+1,n+1):
+            a=string[i:j]
+            arr.append(a)
+    print(arr)
 
 def my_Func():
     pass
